1749ef6881a3c66aed244bfb981a49cd7d54ef69
JoshuaColicchio <joshuacolicchio@gmail.com>
2019-09-18 19:08:19 -0400

    Finished up to week 4 goals.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,52 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField productNameTextField;
+
+    @FXML
+    private TextField manufacturerTextField;
+
+    @FXML
+    private ChoiceBox<?> itemTypeChoiceBox;
+
+    @FXML
+    private Button addProductButton;
+
+    @FXML
+    private TableView<?> existingProductsTableView;
+
+    @FXML
+    private ListView<?> chooseProductListView;
+
+    @FXML
+    private ComboBox<?> chooseQuantityComboBox;
+
+    @FXML
+    private Button recordProductBtn;
+
+    @FXML
+    private TextArea productionLogTextArea;
+
+    @FXML
+    void addProductBtnPushed(ActionEvent event) {
+        System.out.println("addProduct");
+    }
+
+    @FXML
+    void recordProductButtonPushed(ActionEvent event) {
+        System.out.println("recordProduct");
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,52 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
+/**
+ * @brief This is the main driver class for this application.
+ * @author Joshua Colicchio
+ */
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  final String H2_DRIVER = "org.h2.Driver";
+  final String H2_URL = "jdbc:h2:./res/hr";
 
+  final String USER = "";
+  final String PASS = "";
+  Connection con;
+  Statement stmt;
 
-    public static void main(String[] args) {
-        launch(args);
+  /**
+   * The starting point of this JavaFX application.
+   * @param primaryStage The primary stage of the JavaFX application.
+   * @throws Exception
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 800, 400));
+    primaryStage.show();
+
+    connectToDatabase();
+
+  }
+
+  public void connectToDatabase() {
+    try {
+      Class.forName(H2_DRIVER);
+      con = DriverManager.getConnection(H2_URL, USER, PASS);
+      stmt = con.createStatement();
+      stmt.close();
+      con.close();
     }
+    catch (Exception ex) {
+      System.out.println("Eception in connectToDatabase: " + ex.toString());
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
