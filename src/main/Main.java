2c46249cf7bbb3993b73a2e98f83d1db3fce869e
Joshua Colicchio <joshuacolicchio@gmail.com>
2019-09-21 18:36:44 -0400

    Main.java
    Removed all database interaction from Main.java, moved to DatabaseManager.java

---
+++ src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * This is the main driver class for this application.
+ *
+ * @author Joshua Colicchio
+ */
+public class Main extends Application {
+
+  /**
+   * The starting point of this JavaFX application.
+   *
+   * @param primaryStage The primary stage of the JavaFX application.
+   * @throws Exception Throws exception if an error occurs.
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Production Tracker");
+    Scene scene = new Scene(root, 800, 400);
+    scene.getStylesheets().add(Main.class.getResource("stylesheet.css").toExternalForm());
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
